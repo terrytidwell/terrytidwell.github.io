d44efb7c36924009aa84659fff85aff0eaf31a46
Louis Thomas <lthomas@walleyesoftware.com>
2015-06-19 07:36:09 -0500

    Checkpoint unit test work.

---
+++ Assert.js
@@ -0,0 +1,179 @@
+//--------------------------------------------------------------------
+// Useful assertions.
+// Created 2006-07-09 by Louis Thomas
+
+Assert=new Object();
+Require=Assert;
+
+Assert.fail=function(sMessage) {
+    throw new Error(-1, "AssertionFailure: "+sMessage+"\n"+Assert.getStackTrace());
+}
+
+Assert.formatValue=function(value) {
+    if ("string"==typeof value) {
+        return "\""+value+"\"";
+    } else {
+        return ""+value;
+    }
+}
+
+Assert.buildMessageAndFail=function(sRel, left, sLeft, right, /*optional*/sRight) {
+    if ("undefined"==typeof sRight) {
+        Assert.fail("Expected "+sLeft+" "+sRel+" "+Assert.formatValue(right)+", instead "+sLeft+" == "+Assert.formatValue(left)+".");
+    } else {
+        Assert.fail("Expected "+sLeft+" "+sRel+" "+sRight+", instead "+sLeft+" == "+Assert.formatValue(left)+" and "+sRight+" == "+Assert.formatValue(right)+".");
+     }
+}
+
+// BTW, it's impossible to create Assert.isDefined, because if the
+// variable is undefined, an exception will be pre-emptively thrown
+// during the attempt to make the method call
+
+Assert.statementNeverExecuted=function() {
+    Assert.fail("Expected statement never executed.");
+}
+
+Assert.valueNeverOccurs=function(value, sValue) {
+    Assert.fail("Expected "+sValue+" == "+Assert.formatValue(value)+" never occurs.");
+}
+
+Assert.eq=function(left, sLeft, right, /*optional*/sRight) {
+    if (!(left==right)) {
+        Assert.buildMessageAndFail("==", left, sLeft, right, sRight);
+    }
+}
+Assert.neq=function(left, sLeft, right, /*optional*/sRight) {
+    if (!(left!=right)) {
+        Assert.buildMessageAndFail("!=", left, sLeft, right, sRight);
+    }
+}
+
+Assert.gt=function(left, sLeft, right, /*optional*/sRight) {
+    if (!(left>right)) {
+        Assert.buildMessageAndFail(">", left, sLeft, right, sRight);
+    }
+}
+Assert.geq=function(left, sLeft, right, /*optional*/sRight) {
+    if (!(left>=right)) {
+        Assert.buildMessageAndFail(">=", left, sLeft, right, sRight);
+    }
+}
+Assert.lt=function(left, sLeft, right, /*optional*/sRight) {
+    if (!(left<right)) {
+        Assert.buildMessageAndFail("<", left, sLeft, right, sRight);
+    }
+}
+Assert.leq=function(left, sLeft, right, /*optional*/sRight) {
+    if (!(left<=right)) {
+        Assert.buildMessageAndFail("<=", left, sLeft, right, sRight);
+    }
+}
+
+Assert.neqNull=function(value, /*optional*/sValue) { Assert.neq(value, sValue, null); }
+Assert.eqNull=function(value, /*optional*/sValue) { Assert.eq(value, sValue, null); }
+Assert.gtZero=function(value, /*optional*/sValue) { Assert.gt(value, sValue, 0); }
+Assert.geqZero=function(value, /*optional*/sValue) { Assert.geq(value, sValue, 0); }
+Assert.ltZero=function(value, /*optional*/sValue) { Assert.lt(value, sValue, 0); }
+Assert.leqZero=function(value, /*optional*/sValue) { Assert.leq(value, sValue, 0); }
+
+Assert.isString=function(value, /*optional*/sValue) {
+    if ("string"!=typeof value) {
+        Assert.buildMessageAndFail("==", typeof value, "typeof "+sValue, "string");
+    }
+}
+
+Assert.notEmpty=function(value, /*optional*/sValue) {
+    Assert.isString(value, sValue);
+    if (0==value.length) {
+        Assert.buildMessageAndFail(">", value.length, sValue+".length", 0);
+    }
+}
+
+Assert.stringContains=function(sLong, sShort) {
+    Assert.isString(sLong, "sLong");
+    Assert.notEmpty(sShort, "sShort");
+    if (-1==sLong.indexOf(sShort)) {
+        Assert.fail("Expected string \""+sLong+"\" contains string \""+sShort+"\".");
+    }
+}
+
+Assert.instanceOf=function(obj, sObj, type, sType) {
+    if (!(obj instanceof type)) {
+        if ("undefined"==typeof sType) {
+            sType="name" in type?type.name:type;
+        }
+        var sObjType=typeof obj;
+        if ("object"==sObjType) {
+            if (null==obj) {
+                sObjType="(null)";
+            } else if ("constructor" in obj) {
+                if ("name" in obj.constructor) {
+                    sObjType=obj.constructor.name;
+                } else {
+                    sObjType=obj.constructor.toString();
+                    var m=sObjType.match(/function\s*((?:\w+\s*\.\s*)*\w+)\s*\(/i);
+                    if (null!=m) {
+                        sObjType=m[1];
+                    }
+                    sObjType=sObjType.replace(/\s*/g, "");
+                }
+            }
+        }
+        WScript.Echo("d");
+        Assert.fail("Expected "+sObj+" instance of "+sType+", instead "+sObj+" instance of "+sObjType+".");
+    }
+}
+//####################################################################
+
+Assert.getStackTrace=function() {
+    var sStackTrace="";
+
+    var key;
+    var visitedFrames=new Array();
+
+    var frame=Assert.getStackTrace.caller;
+    while (true) {
+        sStackTrace+="    at "+("name" in frame?frame.name:"(anonymous)")+"\n";
+        for (key in visitedFrames) {
+            if (visitedFrames[key]===frame) {
+                sStackTrace+="    ...(recursed)...\n";
+                return sStackTrace;
+            }
+        }
+        visitedFrames.push(frame);
+        frame=frame.caller;
+        if (null==frame) {
+            sStackTrace+="    at (anonymous)\n";
+            return sStackTrace;
+        }
+    }
+}
+
+//--------------------------------------------------------------------
+Assert.fixupMethodNames=function(obj, sClassName) {
+    function fixup() {
+        var key;
+        for (key in obj) {
+            if (obj.hasOwnProperty(key) && obj[key] instanceof Function) {
+                obj[key].name=sClassName+"."+key;
+            }
+        }
+    }
+    fixup();
+    if ("prototype" in obj) {
+        obj=obj.prototype;
+        fixup();
+    }
+}
+
+Assert.fixupMethodNames(Assert, "Assert");
+
+/*
+
+Assert.isNumber        //computing methods
+Assert.isNotNumber
+
+Assert.isEqual        //comparing methods
+Assert.isNotEqual
+
+*/
---
+++ TestingUtil.js
@@ -0,0 +1,191 @@
+//--------------------------------------------------------------------
+// Testing utilities
+// Created 2006-07-09 by Louis Thomas
+
+TestingUtil=new Object();
+
+//--------------------------------------------------------------------
+TestingUtil.test=function(obj) {
+    var key;
+    var nTotal=0;
+    var nSucceeded=0;
+    var nFailed=0;
+    for (key in obj) {
+        if (null!=key.match(/test.*/) && obj[key] instanceof Function) {
+            nTotal++;
+            try {
+                obj[key].call(obj);
+                nSucceeded++;
+            } catch (e) {
+                WScript.Echo("---- "+("name" in obj[key]?obj[key].name:key)+" failed: ----");
+                WScript.Echo(e.description);
+                nFailed++;
+            }
+        }
+    }
+    if (nTotal==nSucceeded) {
+        WScript.Echo("---- All "+nTotal+" tests succeeded. ----");
+    } else {
+        WScript.Echo("---- Some tests failed. Out of "+nTotal+" tests, "+nSucceeded+" succeeded and "+nFailed+" failed. ----");
+    }
+}
+
+//--------------------------------------------------------------------
+/** Dumps the properties of the given object.
+ * The 'constructor' property is not processed for children of
+ * function objects and children of a 'constructor' property.
+ * Inherrited properties will be prefixed by "@".
+ * Un-enumerable properties will be prefixed by "%". (We guess which
+ * un-enumerable properties exist.)
+ * @param obj object to dump.
+ * @param sName name to precede the dump
+ * @param bRecurse [optional, default=true] true if member objects
+ * should be dumped.
+ * @param sPrefix [optional, default=""] string to print in front
+ * of each line. */
+TestingUtil.dumpObj=function (obj, /*optional*/sName, /*optional*/bRecurse, /*optional*/sPrefix) {
+
+    // provide defaults for optional params
+    if ("undefined"==typeof sName) {
+        sName="(an object)";
+    }
+    if ("undefined"==typeof sPrefix) {
+        sPrefix="";
+    }
+    if ("undefined"==typeof bRecurse) {
+        bRecurse=true;
+    }
+
+    if ("object"!=typeof obj && "function"!=typeof obj) {
+        WScript.Echo(sPrefix+sName+": (Not an object! Type:"+(typeof obj)+")");
+    } else if (null==obj) {
+        WScript.Echo(sPrefix+sName+": (null)");
+    } else {
+        WScript.Echo(sPrefix+sName+":");
+        dumpObjImpl(obj, bRecurse, sPrefix, false);
+    }
+    return;
+
+    //----------------------------------------------------------------
+    function printKey(obj, sKey, bRecurse, sPrefix, sPrefix2, bSkipCtor, bHidden) {
+        var sType=typeof obj[sKey];
+        var sIntPrefix="";
+        if (("hasOwnProperty" in obj) && !obj.hasOwnProperty(sKey)) {
+            sIntPrefix+="@";
+        }
+        if (bHidden) {
+            sIntPrefix+="%";
+        }
+        switch (sType) {
+
+        case "undefined":
+        // don't print this property
+            bRecurse=false;
+            break;
+
+        case "function":
+            var sFunc=obj[sKey].toString();
+            var rgMatches=sFunc.match(/function\s*((?:\w+\s*\.\s*)*\w+)\s*\(/i);
+            var sFuncName;
+            if (null==rgMatches) {
+                sFuncName=sFunc;
+            } else {
+                sFuncName=rgMatches[1].replace(/\s*/g, "");
+            }
+            WScript.Echo(sPrefix+sIntPrefix+sKey+"=["+sType+"] "+sFuncName);
+            bSkipCtor=true;
+            break;
+
+        case "string":
+            WScript.Echo(sPrefix+sIntPrefix+sKey+"=["+sType+"] \""+obj[sKey]+"\"");
+            bRecurse=false; // literals can't have expandos
+            break;
+
+        case "number":
+        case "boolean":
+            WScript.Echo(sPrefix+sIntPrefix+sKey+"=["+sType+"] "+obj[sKey]);
+            bRecurse=false; // literals can't have expandos
+            break;
+
+        case "object":
+            if (null==obj[sKey]) {
+                WScript.Echo(sPrefix+sIntPrefix+sKey+"=null");
+                bRecurse=false;
+            } else {
+
+                WScript.Echo(sPrefix+sIntPrefix+sKey+"=["+sType+"]");
+            }
+            break;
+
+        default:
+            throw new Error(-1, "unknown type '"+sType+"'.");
+        }
+        if (bRecurse) {
+            dumpObjImpl(obj[sKey], bRecurse, sPrefix2, bSkipCtor);
+        }
+    }
+
+    //----------------------------------------------------------------
+    function dumpObjImpl(obj, bRecurse, sPrefix, bSkipCtor) {
+
+        var rgKnownProps=new Array(
+        // skip the dynamic properties, because of possibility of unbounded recursion
+        //"arguments", "callee", "caller",
+            "constructor", "description", "E", "global",
+            "ignoreCase", "index", "Infinity", "input",
+            "lastIndex", "leftContext", "length", "lastMatch", "lastParen",
+            "LN2", "LN10", "LOG2E", "LOG10E", "MAX_VALUE", "message",
+            "MIN_VALUE", "multiline", "NaN", "name", "NEGATIVE_INFINITY",
+            "number", "PI", "POSITIVE_INFINITY",
+            "prototype", "source", "SQRT1_2", "SQRT_2", "undefined");
+
+        // first, see which of the un-enumerable props we have
+        var rgProps=new Array();
+        var nIndex;
+        for (nIndex=0; nIndex<rgKnownProps.length; nIndex++) {
+            if (rgKnownProps[nIndex] in obj) {
+                rgProps.push(rgKnownProps[nIndex]);
+            }
+        }
+
+        // second, see which enumerable props we have
+        var rgProps2=new Array();
+        var sKey;
+        for (sKey in obj) {
+            rgProps2.push(sKey);
+
+            // remove enumerable props from un-enumerable props
+            for (nIndex=0; nIndex<rgProps.length; nIndex++) {
+                if (sKey==rgProps[nIndex]) {
+                    rgProps.splice(nIndex, 1);
+                    break;
+                }
+            }
+        }
+
+        // combine the lists
+        var nHiddenCount=rgProps.length;
+        var bHidden;
+        rgProps=rgProps.concat(rgProps2);
+
+        // print each element
+        var sPrefix1=sPrefix+"|---";
+        var sPrefix2=sPrefix+"|   ";
+        for (nIndex=0; nIndex<rgProps.length; nIndex++) {
+            bHidden=nIndex<nHiddenCount;
+            if (nIndex+1==rgProps.length) {
+                sPrefix1=sPrefix+"\\---";
+                sPrefix2=sPrefix+"    ";
+            }
+            if ("constructor"!=rgProps[nIndex]) {
+                printKey(obj, rgProps[nIndex], bRecurse, sPrefix1, sPrefix2, bSkipCtor, bHidden);
+            } else if (false==bSkipCtor) {
+                printKey(obj, "constructor", bRecurse, sPrefix1, sPrefix2, true, bHidden);
+            } else {
+                //WScript.Echo("skip ctor"); // skipped inside functions and ctors
+            }
+        }
+    }
+}
+
+Assert.fixupMethodNames(TestingUtil, "TestingUtil");
