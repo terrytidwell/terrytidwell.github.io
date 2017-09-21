a5ae0aaa3d62a5651dc7765379564e77336c1815
TheAks999 <aksfkb@mst.edu>
2017-09-20 20:28:30 -0500

    did some changes to PacsiOs to make it instantiable instead of global. Added some resources for the system

--- pacsi/js/system.js
+++ pacsi/js/system.js
@@ -1,27 +1,28 @@
-var ResourcesCmd= function(run_context)
+var ResourcesCmd = function(run_context)
 {
   this.run_context = run_context;
   this.parent_window = run_context.parent_window;
   this.last_input = null;
-  this.parent_window.accept_input = true;
+  this.parent_window.accept_input = false;
   this.resources = null;
   //reserve space to write stuff
   this.parent_window.handleOutput(" ");
   
-  this.handleInput = function(string)
-  {
-    this.last_input = string;
-  }
-  
   this.handleTimeStep = function()
   {
-    if (this.last_input === 'q')
-    {
-      return false;
-    }
-    
-    string = "";
+    var string = this.formatResourcesForReplicant(this.run_context.os);
     this.parent_window.overrideOutput(string);
-    return true;
+    return false;
+  }
+  
+  this.formatResourcesForReplicant = function(os)
+  {
+	  var number_of_running_programs = os.programs.length;
+	  // FIXME
+	  return "Resources:\n" +
+		"-Running Programs:\t" + number_of_running_programs + " / " + os.resources.max_programs + "\n" +
+	    "-Storage:\t" + os.resources.storage_space_used + " / " + os.resources.storage_space_total + "\n" +
+	    "-Memory:\t" + os.resources.memory_used + " / " + os.resources.memory_total + "\n"  +
+	    "-Peripheral Devices:\t" + os.resources.peripheral_devices.length + "\n";
   }
 }
