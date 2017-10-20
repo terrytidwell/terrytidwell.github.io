27f011292d40c58948199885a69090bec568d606
terrytidwell <terrytidwell@gmail.com>
2017-10-19 22:53:38 -0500

    It Runs!

--- gameEngine/engine.js
+++ gameEngine/engine.js
@@ -1,9 +1,11 @@
 const JutGameEngine = (function() {
-  var canvas = document.createElement('canvas');
-  var current_screen = null;
-  this.ctx = g_canvas.getContext("2d");
+  var canvas = document.getElementById("canvas");
+  var current_screen = resourceTracker;
+  var ctx = canvas.getContext("2d");
+  var FPS = 25;
+  var MILLIS_PER_SECOND = 1000;
   
-  this.resize = function ()
+  var resize = function ()
   {
     var widthToHeightRation = 9/16;
     var currentWidthToHeightRatio = window.innerWidth/window.innerHeight;
@@ -24,11 +26,96 @@ const JutGameEngine = (function() {
     
     if (current_screen)
     {
-      current_screen.paint(g_ctx);
+      current_screen.paint(canvas, ctx);
     }
-  },
+  };
   
-  this.init = function()
+  var loadImage = function (url)
+  {
+    var image = new Image();
+    resourceTracker.add();
+    image.onload = function (event)
+    {
+      resourceTracker.onload(event);
+    };
+    image.src = url;
+    return image;
+  };
+
+  var loadAudio = function (url)
+  {
+    var audio = new buzz.sound(
+      url, {
+        //formats: [ "wav" ],
+        preload: true,
+        autoplay: false,
+        loop: false
+      });
+    resourceTracker.add();
+    audio.bindOnce("canplay", function (event)
+    {
+      resourceTracker.onload(event)
+    });
+    return audio;
+  };
+  
+  var resourceTracker = {
+    m_added: 1, // I track myself loading
+    m_loaded: 0,
+
+    init: function ()
+    {
+        this.onload(null); //and here I'm loaded
+    },
+
+    handleTimeStep: function ()
+    {
+      if (this.m_loaded == this.m_added)
+      {
+        this.onAllLoaded();
+      }
+    },
+
+    paint: function (canvas, ctx)
+    {
+      ctx.setTransform(1, 0, 0, 1, 0, 0);
+
+      // paint the background
+      ctx.fillStyle = "rgb(255, 255, 255)";
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+      ctx.font="Bold 80px Arial";
+      ctx.fillStyle = "rgb(92, 92, 92)";
+      ctx.textAlign = "center";
+      ctx.textBaseline = "middle";
+      ctx.fillText(
+        Math.floor(100 * this.m_loaded / this.m_added) + "%",
+        canvas.width / 2,
+        canvas.height / 2
+      );
+    },
+
+    add: function ()
+    {
+      ++this.m_added;
+    },
+
+    onload: function (event)
+    {
+      ++this.m_loaded;
+      if (this.m_loaded == this.m_added)
+      {
+        this.onAllLoaded();
+      }
+    },
+
+    onAllLoaded: function ()
+    {
+      switchToScreen(GameScreen);
+    }
+  };
+  
+  var init = function()
   {
     document.addEventListener('wheel', function(event) {
       if(current_screen.handleMouseWheel)
@@ -86,10 +173,47 @@ const JutGameEngine = (function() {
       }
     }, false);
     
-    window.addEventListener('resize', this.resize, false);
-    window.addEventListener('orientationchange', this.resize, false);
-    this.resize();
+    window.addEventListener('resize', resize, false);
+    window.addEventListener('orientationchange', resize, false);
+    resize();
 
+    resourceTracker.init();
+    
+    current_screen = resourceTracker;
+
+    // start processing events
+    setTimeout(eventLoop, 40);
+  };
+  
+  var eventLoop = function()
+  {
+    var start_time = Date.now();
+
+    current_screen.handleTimeStep();
+    current_screen.paint(canvas, ctx);
+
+    var end_time = Date.now();
+    var comp_time = end_time - start_time;
+    if (comp_time > (MILLIS_PER_SECOND / FPS) || comp_time < 0) {
+      setTimeout(eventLoop, 0);
+    }
+    else
+    {
+      setTimeout(eventLoop, (MILLIS_PER_SECOND / FPS) - comp_time);
+    }
   }
+  
+  var switchToScreen = function(screen)
+  {
+    screen.reset();
+    current_screen = screen;
+  };
+  
+  return {
+    init : init,
+    switchToScreen : switchToScreen,
+    loadAudio : loadAudio,
+    loadImage : loadImage
+  };
   
 }());
