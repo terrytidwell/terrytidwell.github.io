430d2f81a002e9401f54f7a96b8dc9b3620f590d
terrytidwell <terrytidwell@gmail.com>
2018-09-29 21:10:24 -0500

    Checking in bonus puzzle

--- crosswordpuzzle/chapters.js
+++ crosswordpuzzle/chapters.js
@@ -1,5 +1,7 @@
 var g_chapters =
 [
   {puzzles: [0]},
-  {puzzles: [1, 2, 3, 4, 5]},
+  {puzzles: [1, 2, 3], 
+    final_puzzle:{"pieces":[{"str":["DE"],"x":4,"y":4,"vx":7,"vy":6},{"str":["TE","H.","E."],"x":5,"y":10,"vx":9,"vy":6},{"str":["CT"],"x":16,"y":4,"vx":11,"vy":6},{"str":["K..","IVE"],"x":17,"y":7,"vx":13,"vy":5},{"str":["D","N"],"x":2,"y":9,"vx":13,"vy":7},{"str":["A","P"],"x":15,"y":11,"vx":13,"vy":9}],"grid":{"str":["......O..","OOOOOOOOO","..O...O..","..O...O..","......O..","......O.."],"x":7,"y":5},"name":"WHAT CRIME WILL I COMMIT?"}
+  },
 ];
