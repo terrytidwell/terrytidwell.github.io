faf979808922784f514e36dcad687933c756554d
terrytidwell <terrytidwell@gmail.com>
2018-10-07 23:57:07 -0500

    Making unlockable puzzles a thing

--- crosswordpuzzle/chapters.js
+++ crosswordpuzzle/chapters.js
@@ -1,7 +1,15 @@
 var g_chapters =
 [
-  {puzzles: [0]},
-  {puzzles: [1, 2, 3], 
-    final_puzzle:{"pieces":[{"str":["DE"],"x":4,"y":4,"vx":7,"vy":6},{"str":["TE","H.","E."],"x":5,"y":10,"vx":9,"vy":6},{"str":["CT"],"x":16,"y":4,"vx":11,"vy":6},{"str":["K..","IVE"],"x":17,"y":7,"vx":13,"vy":5},{"str":["D","N"],"x":2,"y":9,"vx":13,"vy":7},{"str":["A","P"],"x":15,"y":11,"vx":13,"vy":9}],"grid":{"str":["......O..","OOOOOOOOO","..O...O..","..O...O..","......O..","......O.."],"x":7,"y":5},"name":"WHAT CRIME WILL I COMMIT?"}
+  {puzzles: [0],
+    final_puzzle:false,
+    name: "PRELUDE",
+  },
+  {puzzles: [1, 2, 3, 32], 
+    final_puzzle:true,
+    name: "THE GAME IS AFOOT",
+  },
+  {puzzles: [4, 5, 6, 7], 
+    final_puzzle:false,
+    name: "THIRD"
   },
 ];
