d9c206026d7db6403d16bafdda33d69bb848ee4f
mjhall3 <mjhall3@fastmail.net>
2019-12-06 18:07:48 -0600

    Detect game width and height.  Render a platform sprite at the bottom.

--- velocigames/dragons/scratch/scene/main.js
+++ velocigames/dragons/scratch/scene/main.js
@@ -17,12 +17,15 @@ let GameScene = new Phaser.Class({
 
     create: function ()
     {
-        this.add.image(400, 300, 'bg');
+        let game_width = this.game.config.width;
+        let game_height = this.game.config.height;
+
+        this.add.image(game_width / 2, game_height / 2, 'bg');
 
         for (let i = 0; i < 64; i++)
         {
-            let x = Phaser.Math.Between(0, 800);
-            let y = Phaser.Math.Between(0, 600);
+            let x = Phaser.Math.Between(0, game_width);
+            let y = Phaser.Math.Between(0, game_height);
 
             let box = this.add.image(x, y, 'crate');
 
@@ -58,10 +61,20 @@ let UIScene = new Phaser.Class({
             this.score = 0;
         },
 
+    preload: function ()
+    {
+        this.load.image('platform', '../../assets/platform.png');
+    },
+
     create: function ()
     {
+        let game_width = this.game.config.width;
+        let game_height = this.game.config.height;
+
         //  Our Text object to display the Score
         let info = this.add.text(10, 10, 'Score: 0', { font: '48px Arial', fill: '#000000' });
+
+        let platform = this.add.sprite(game_width / 2, game_height - 50, "platform");
 
         //  Grab a reference to the Game Scene
         let ourGame = this.scene.get('GameScene');
@@ -82,7 +95,7 @@ let config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
-    backgroundColor: '#000000',
+    backgroundColor: '#800000',
     parent: 'phaser-example',
     scene: [ GameScene, UIScene ]
 };
