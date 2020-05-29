f1836a389377c4e3c67fbb5868ce46c0ddb80f07
terrytidwell <terrytidwell@users.noreply.github.com>
2020-05-29 12:37:21 -0500

    Adding a broken state to tiles

--- stoneheart/js/main.js
+++ stoneheart/js/main.js
@@ -25,14 +25,14 @@ let  StartScreen = new Phaser.Class({
     create: function () {
         let screen = this;
         screen.input.addPointer(5);
-        screen.me_state = 5;
+        screen.me_state = 10;
         screen.grid = [];
         screen.me_x = 0;
         screen.me_y = 0;
         for (let x = 0; x < SCREEN_COLUMNS; x++) {
             screen.grid.push([]);
             for (let y = 0; y < SCREEN_ROWS; y++) {
-                let value = Math.floor(Math.random() * 6)
+                let value = Math.floor(Math.random() * 5);
                 if (x == screen.me_x && y == screen.me_y && value == 5)
                 {
                     value--;
@@ -40,6 +40,7 @@ let  StartScreen = new Phaser.Class({
                 if (value != 5) {
                     screen.grid[x].push({
                         valid: true,
+                        broken: false,
                         value:value,
                         sprite: screen.add.tileSprite(
                             x * GRID_SIZE + GRID_SIZE/2,
@@ -48,6 +49,7 @@ let  StartScreen = new Phaser.Class({
                 } else {
                     screen.grid[x].push({
                         valid: false,
+                        broken: false,
                         value: value,
                         sprite: null});
                 }
@@ -65,16 +67,31 @@ let  StartScreen = new Phaser.Class({
         screen.me_border.push(screen.add.sprite(0,0,'corner'));
         screen.me_border[3].flipX = true;
         screen.me_border[3].flipY = true;
+        let set_border = function(bool) {
+            screen.me_border[0].visible = bool;
+            screen.me_border[1].visible = bool;
+            screen.me_border[2].visible = bool;
+            screen.me_border[3].visible = bool;
+        }
+
         let toggle_border = function() {
-            screen.me_border[0].visible = !screen.me_border[0].visible;
-            screen.me_border[1].visible = !screen.me_border[1].visible;
-            screen.me_border[2].visible = !screen.me_border[2].visible;
-            screen.me_border[3].visible = !screen.me_border[3].visible;
+            set_border(!screen.me_border[0].visible);
         };
-        toggle_border();
+        set_border(false);
+
+        let is_border_active = function() {
+            return screen.me_border[0].visible;
+        }
+
+        screen.me_sprite = screen.add.tileSprite(
+            0, 0, GRID_SIZE, GRID_SIZE, 'blocks',
+            screen.grid[screen.me_x][screen.me_y].value + screen.me_state);
 
         let align_border = function(x, y)
         {
+            screen.me_sprite.x = x * GRID_SIZE + GRID_SIZE/2;
+            screen.me_sprite.y = y * GRID_SIZE + GRID_SIZE/2;
+
             screen.me_border[0].x = x * GRID_SIZE + CORNER_OFFSET_SMALLER;
             screen.me_border[0].y = y * GRID_SIZE + CORNER_OFFSET_BIGGER;
             screen.me_border[1].x = x * GRID_SIZE + CORNER_OFFSET_BIGGER;
@@ -86,27 +103,38 @@ let  StartScreen = new Phaser.Class({
         };
         align_border(screen.me_x, screen.me_y);
 
-        screen.grid[screen.me_x][screen.me_y].sprite.setTexture('blocks', screen.grid[screen.me_x][screen.me_y].value + screen.me_state);
+
         screen.time.addEvent({
             "delay": 750,
             "loop": true,
             "callback": function () {
-                if (screen.me_state == 5) {
-                    screen.me_state = 10;
+                if (screen.me_state == 10) {
+                    screen.me_state = 15;
                 } else {
-                    screen.me_state = 5;
+                    screen.me_state = 10;
                 }
-                screen.grid[screen.me_x][screen.me_y].sprite.setTexture('blocks', screen.grid[screen.me_x][screen.me_y].value + screen.me_state);
+                screen.me_sprite.setTexture('blocks',
+                    screen.grid[screen.me_x][screen.me_y].value + screen.me_state);
             }
         });
+
+        let set_block_texture = function(x,y)
+        {
+            let texture = screen.grid[x][y].value;
+            if (screen.grid[x][y].broken)
+            {
+                texture += 5;
+            }
+            screen.grid[x][y].sprite.setTexture('blocks', texture);
+        }
 
         let move_character = function(delta_x, delta_y)
         {
             if (screen.me_x + delta_x < 0 ||
-                screen.me_x + delta_x > screen.grid.length ||
+                screen.me_x + delta_x > screen.grid.length - 1 ||
                 screen.me_y + delta_y < 0 ||
-                screen.me_y + delta_y > screen.grid[screen.me_x + delta_x].length ||
-                !screen.grid[screen.me_x + delta_x][screen.me_y + delta_y].valid
+                screen.me_y + delta_y > screen.grid[screen.me_x + delta_x].length - 1 || //bounds check
+                !screen.grid[screen.me_x + delta_x][screen.me_y + delta_y].valid //valid block check
             )
             {
                 //no move
@@ -118,15 +146,74 @@ let  StartScreen = new Phaser.Class({
             screen.me_x += delta_x;
             screen.me_y += delta_y;
             let new_value = screen.grid[screen.me_x][screen.me_y].value;
-            if (screen.me_border[0].visible)
+            if (is_border_active())
             {
                 screen.grid[old_x][old_y].value = new_value;
                 screen.grid[screen.me_x][screen.me_y].value = old_value;
             }
-            screen.grid[old_x][old_y].sprite.setTexture('blocks', screen.grid[old_x][old_y].value);
-            screen.grid[screen.me_x][screen.me_y].sprite.setTexture('blocks', screen.grid[screen.me_x][screen.me_y].value + screen.me_state);
+            set_block_texture(old_x,old_y);
+            set_block_texture(screen.me_x, screen.me_y);
+            screen.me_sprite.setTexture('blocks', screen.grid[screen.me_x][screen.me_y].value + screen.me_state)
             align_border(screen.me_x, screen.me_y);
+            clear_matching();
         };
+
+        let clear_matching = function() {
+            //return false;
+            let to_delete = [];
+            for (let i = 0; i < SCREEN_COLUMNS; i++) {
+                let current_run_ij = 1
+                let previous_value_ij = -1;
+                let current_run_ji = 1
+                let previous_value_ji = -1;
+                for (let j = 0; j < SCREEN_ROWS; j++) {
+                    if (screen.grid[i][j].value == previous_value_ij) {
+                        current_run_ij++;
+                        if (current_run_ij == 3) {
+                            to_delete.push([i,j - 2]);
+                            to_delete.push([i,j - 1]);
+                        }
+                        if (current_run_ij >= 3) {
+                            to_delete.push([i,j]);
+                        }
+                    }
+                    else {
+                        previous_value_ij = screen.grid[i][j].value;
+                        current_run_ij = 1;
+                    }
+                    if (screen.grid[j][i].value == previous_value_ji) {
+                        current_run_ji++;
+                        if (current_run_ji == 3) {
+                            to_delete.push([j - 2,i]);
+                            to_delete.push([j - 1,i]);
+                        }
+                        if (current_run_ji >= 3) {
+                            to_delete.push([j,i]);
+                        }
+                    }
+                    else {
+                        previous_value_ji = screen.grid[j][i].value;
+                        current_run_ji = 1;
+                    }
+                }
+            }
+            for (let i = 0; i < to_delete.length; i++) {
+                let value = Math.floor(Math.random() * 5);
+                if (!screen.grid[to_delete[i][0]][to_delete[i][1]].broken) {
+                    screen.grid[to_delete[i][0]][to_delete[i][1]].broken = true;
+                    set_block_texture(to_delete[i][0],to_delete[i][1]);
+                }
+
+                screen.grid[to_delete[i][0]][to_delete[i][1]].valid = false;
+                if (to_delete[i][0] == screen.me_x && to_delete[i][1] == screen.me_y) {
+                    set_border(false);
+                }
+                //screen.grid[to_delete[i][0]][to_delete[i][1]].sprite.visible = false;
+            }
+            return to_delete.length != 0;
+        };
+
+        clear_matching();
 
         screen.m_cursor_keys = screen.input.keyboard.createCursorKeys();
         screen.m_cursor_keys.down.on('down', function(event) {
@@ -138,8 +225,18 @@ let  StartScreen = new Phaser.Class({
         screen.m_cursor_keys.right.on('down', function(event) {
             move_character(1,0)});
 
+        let try_selection = function()
+        {
+            if (!is_border_active() && screen.grid[screen.me_x][screen.me_y].broken)
+            {
+                return;
+            }
+
+            toggle_border();
+        }
+
         screen.space_key = screen.input.keyboard.addKey("a");
-        screen.space_key.on('down', toggle_border);
+        screen.space_key.on('down', try_selection);
     },
 
     update: function () {
