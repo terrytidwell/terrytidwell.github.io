4d76d4e194e82908ac17106f3c0ff543b96a1b3d
terrytidwell <terrytidwell@users.noreply.github.com>
2020-06-22 14:36:32 -0500

    Added music, title card

--- mybandequalsme/js/main.js
+++ mybandequalsme/js/main.js
@@ -1,5 +1,50 @@
-const SCREEN_WIDTH = 512;
-const SCREEN_HEIGHT = 256;
+const SCREEN_WIDTH = 1024;
+const SCREEN_HEIGHT = 512;
+
+let TitleScene = new Phaser.Class({
+
+    Extends: Phaser.Scene,
+
+    //--------------------------------------------------------------------------
+    initialize: function () {
+        Phaser.Scene.call(this, {key: 'TitleScene', active: true});
+    },
+
+    //--------------------------------------------------------------------------
+    preload: function () {
+        this.load.spritesheet('fighter_idle', 'assets/fighter_idle.png', { frameWidth: 128, frameHeight: 128});
+        this.load.audio('qtclash', ['assets/qtclash.mp3']);
+    },
+
+    //--------------------------------------------------------------------------
+    create: function () {
+        let scene = this;
+        let play = scene.add.text(
+            SCREEN_WIDTH/2,
+            SCREEN_HEIGHT/2,
+            "click to continue...",
+            { font: 32 + 'px Arial', color: '#ffffff'})
+            .setOrigin(0.5, 0.5)
+            .setStroke('#ffffff', 3);
+        play.setInteractive();
+        play.on(Phaser.Input.Events.GAMEOBJECT_POINTER_OVER, function(){
+            play.scaleX = 1.2;
+            play.scaleY = 1.2;
+        });
+        play.on(Phaser.Input.Events.GAMEOBJECT_POINTER_OUT, function(){
+            play.scaleX = 1;
+            play.scaleY = 1;
+        });
+        play.on(Phaser.Input.Events.GAMEOBJECT_POINTER_UP, function(){
+            scene.scene.start('UIScene');
+            scene.scene.start('GameScene');
+        });
+    },
+
+    //--------------------------------------------------------------------------
+    update: function() {
+    },
+});
 
 let GameScene = new Phaser.Class({
 
@@ -7,12 +52,11 @@ let GameScene = new Phaser.Class({
 
     //--------------------------------------------------------------------------
     initialize: function () {
-        Phaser.Scene.call(this, {key: 'GameScene', active: true});
+        Phaser.Scene.call(this, {key: 'GameScene', active: false});
     },
 
     //--------------------------------------------------------------------------
     preload: function () {
-        this.load.spritesheet('fighter_idle', 'assets/fighter_idle.png', { frameWidth: 128, frameHeight: 128});
     },
 
     //--------------------------------------------------------------------------
@@ -47,9 +91,6 @@ let GameScene = new Phaser.Class({
             repeat: 0
         });
 
-
-
-
         let damage = function()
         {
             players[0].play('hit')
@@ -72,40 +113,57 @@ let GameScene = new Phaser.Class({
                 duration: 100
             });
         }
+
         players.push(
-            scene.add.sprite(SCREEN_WIDTH/2 - 96, SCREEN_HEIGHT, 'fighter_idle', 0)
+            scene.add.sprite(SCREEN_WIDTH/2 - 192, SCREEN_HEIGHT, 'fighter_idle', 0)
                 .setOrigin(0.5, 1)
-                .setScale(2)
+                .setScale(4)
                 .play('idle'));
         players.push(
-            scene.add.sprite(SCREEN_WIDTH/2 + 96, SCREEN_HEIGHT, 'fighter_idle', 0)
+            scene.add.sprite(SCREEN_WIDTH/2 + 192, SCREEN_HEIGHT, 'fighter_idle', 0)
                 .setOrigin(0.5, 1)
-                .setScale(2)
+                .setScale(4)
                 .setFlipX(true)
                 .play('idle'));
-        scene.add.rectangle(SCREEN_WIDTH/2 - 16, 32, 512/2 - 64, 18, 0xffff00,0)
+        let life_x = SCREEN_WIDTH/2;
+        let life_x_offset = 32;
+        let life_y = 64;
+        let life_w = SCREEN_WIDTH/2 - 128;
+        let life_h = 32;
+        let pinstripe = 4;
+
+        scene.add.rectangle(life_x - life_x_offset + pinstripe/2, life_y,
+            life_w + pinstripe, life_h + pinstripe, 0xffff00,0)
             .setOrigin(1,0.5)
-            .setStrokeStyle(2,0xffffff,1);
-        scene.add.rectangle(SCREEN_WIDTH/2 + 16, 32, 512/2 - 64, 18, 0xffff00, 0)
+            .setStrokeStyle(pinstripe,0xffffff,1);
+        scene.add.rectangle(life_x + life_x_offset - pinstripe/2, life_y,
+            life_w + pinstripe, life_h + pinstripe, 0xffff00,0)
             .setOrigin(0,0.5)
-            .setStrokeStyle(2,0xffffff,1);
+            .setStrokeStyle(pinstripe,0xffffff,1);
         let life_bg = [];
         life_bg.push(
-            scene.add.rectangle(SCREEN_WIDTH/2 - 16, 32, 512/2 - 64, 16, 0xff0000)
+            scene.add.rectangle(life_x - life_x_offset, life_y,
+                life_w, life_h, 0xff0000)
                 .setOrigin(1,0.5));
         life_bg.push(
-            scene.add.rectangle(SCREEN_WIDTH/2 + 16, 32, 512/2 - 64, 16, 0xff0000)
+            scene.add.rectangle(life_x + life_x_offset, life_y,
+                life_w, life_h, 0xff0000)
                 .setOrigin(0,0.5));
         let life = [];
         life.push(
-            scene.add.rectangle(SCREEN_WIDTH/2 - 16, 32, 512/2 - 64, 16, 0xffff00)
+            scene.add.rectangle(life_x - life_x_offset, life_y,
+                life_w, life_h, 0xffff00)
             .setOrigin(1,0.5));
         life.push(
-            scene.add.rectangle(SCREEN_WIDTH/2 + 16, 32, 512/2 - 64, 16, 0xffff00)
+            scene.add.rectangle(life_x + life_x_offset, life_y,
+                life_w, life_h, 0xffff00)
             .setOrigin(0,0.5))
 
         scene.space_key = scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
         scene.space_key.on('down', damage);
+
+        scene.sound.pauseOnBlur = false;
+        scene.sound.add('qtclash', {loop: true }).play();
     },
 
     //--------------------------------------------------------------------------
@@ -133,7 +191,7 @@ let config = {
             debug: false
         }
     },
-    scene: [ GameScene ]
+    scene: [ TitleScene, GameScene ]
 };
 
 game = new Phaser.Game(config);
