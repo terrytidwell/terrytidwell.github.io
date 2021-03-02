b714d98a0bd4fafc81a5f21afbdb0b44735dd5d6
terrytidwell <terrytidwell@users.noreply.github.com>
2021-03-02 01:36:08 -0600

    Pawn logic filled in

--- knightstale/js/main.js
+++ knightstale/js/main.js
@@ -4,6 +4,11 @@ const GRID_COLS = 12;
 const GRID_ROWS = 12;
 const SCREEN_WIDTH = GRID_SIZE * GRID_COLS; //845 + 400; //845; //1025
 const SCREEN_HEIGHT = GRID_SIZE * GRID_COLS; //542 + 150; //542; //576
+const DEPTHS = {
+    BOARD: 0,
+    SURFACE: 1,
+    ENTITIES: 1000,
+}
 
 let LoadScene = new Phaser.Class({
 
@@ -211,6 +216,7 @@ let GameScene = new Phaser.Class({
                 let offset = 4 * ((x + y) % 2);
                 let tile = Phaser.Utils.Array.Shuffle(index_image)[0] + offset;
                 let square = scene.add.sprite(gridX(x), gridY(y), 'floor', tile);
+                square.setDepth(DEPTHS.BOARD);
                 square.setVisible(false);
                 if (x >= 2 && x < 10 &&
                     y >= 2 && y < 10)
@@ -256,6 +262,7 @@ let GameScene = new Phaser.Class({
             frame.setData('dx', moves[i].dx);
             frame.setData('dy', moves[i].dy);
             frame.setInteractive();
+            frame.setDepth(DEPTHS.SURFACE);
             frame.on(Phaser.Input.Events.GAMEOBJECT_POINTER_DOWN,
                 function () {
                     tryMovePlayer(playerX + frame.data.values.dx,
@@ -266,7 +273,9 @@ let GameScene = new Phaser.Class({
 
         scene.__alignPlayer = function() {
             character.setPosition(gridX(playerX), characterY(playerY)+playerZ);
+            character.setDepth(DEPTHS.ENTITIES + playerY);
             shadow.setPosition(gridX(playerX),gridY(playerY));
+            bounding_box.setPosition(gridX(Math.round(playerX)),gridY(Math.round(playerY)));
             impact_sprite.setPosition(gridX(playerX),gridY(playerY));
             for (let i = 0; i < frames.length; i++) {
                 let frame = frames[i];
@@ -278,64 +287,244 @@ let GameScene = new Phaser.Class({
                     isGridPassable(x,y));
             }
         };
-
+        let bounding_box = scene.add.rectangle(0,0,GRID_SIZE-2,GRID_SIZE-2,0x00ff00,0.0);
         let impact_sprite = scene.add.sprite(0,0, 'impact', 5).setVisible(true).setScale(2);
         let shadow = scene.add.ellipse(0, 0,
             GRID_SIZE*.70,GRID_SIZE*.57,0x000000, 0.5);
+        shadow.setDepth(DEPTHS.SURFACE);
         let character = scene.add.sprite(0, 0, 'pieces', 4);
+
         let addPawn = function(x,y) {
             let pawn = scene.add.sprite(gridX(0), characterY(0), 'pieces', 12);
-            let mx = x;
-            let my = y;
-            let mdx = 0;
-            let mdy = 0;
-            let timelineX = null;
-            let timelineY = null;
-            let delayedCall = null;
-            let begin_walk = function() {
-                let directions = [
-                    {dx: -1, dy: 0},
-                    {dx: 1, dy: 0},
-                    {dx: 0, dy: -1},
-                    {dx: 0, dy: 1},
-                ];
-                Phaser.Utils.Array.Shuffle(directions);
-                mdx = 0;
-                mdy = 0;
-                for (let direction of directions) {
-                    if (isGridPassable(mx+direction.dx, my+direction.dy)) {
+            let m_x = x;
+            let m_y = y;
+            let m_z = 0;
+            let m_dx = 0;
+            let m_dy = 0;
+            let m_prefer_horizontal = Phaser.Math.Between(0,99) < 50;
+            //cancel tweens with tween.remove();
+            //cancel timeline with timeline.stop();
+            //cancel with delayedCall.remove(false) <-- if you don't want callback
 
-                        mdx = direction.dx;
-                        mdy = direction.dy;
-                        scene.tweens.add({
-                            targets: { x: mx},
-                            props: { x: mx+mdx },
-                            duration: 200,
-                            onUpdate: function() {
-                                mx = this.getValue();
-                            },
-                        });
-                        scene.tweens.add({
-                            targets: { y: my},
-                            props: { y: my+mdy },
-                            duration: 200,
-                            onUpdate: function() {
-                                my = this.getValue();
-                            },
-                            onComplete: function() {
-                                delayedCall = scene.time.delayedCall(1000, begin_walk);
+            let tweens = [];
+            let timelines = [];
+            let delayedCalls = [];
+
+            let STATES = {
+                MOVING: 0,
+                IDLE: 1,
+                PRE_ATTACK: 2,
+                ATTACK: 3,
+                POST_ATTACK_IDLE: 4
+            };
+            let current_state = STATES.IDLE;
+
+            let enter_state = function() {
+                switch(current_state) {
+                    case STATES.MOVING:
+                        enter_move();
+                        break;
+                    case STATES.IDLE:
+                        delayedCalls.push(scene.time.delayedCall(1000, change_state, [STATES.MOVING]));
+                        break;
+                      case STATES.POST_ATTACK_IDLE:
+                        delayedCalls.push(scene.time.delayedCall(1000, change_state, [STATES.MOVING]));
+                        break;
+                    case STATES.PRE_ATTACK:
+                        tweens.push(scene.tweens.add({
+                                targets: {z : 0},
+                                props: {z: -8},
+                                yoyo: true,
+                                duration: 200,
+                                onUpdate: function() {
+                                    m_z = this.getValue();
+                                },
+                                onComplete: function () {
+                                    change_state(STATES.ATTACK);
+                                },
+                        }));
+                        break;
+                    case STATES.ATTACK:
+                        let deltaX = Math.round(playerX) - Math.round(m_x);
+                        let deltaY = Math.round(playerY) - Math.round(m_y);
+                        if ((deltaX === -1 && deltaY === -1) ||
+                                (deltaX === 1 && deltaY === 1) ||
+                                (deltaX === 1 && deltaY === -1) ||
+                                (deltaX === -1 && deltaY === 1)) {
+                                m_dx = deltaX;
+                                m_dy = deltaY;
+                                if (isGridPassable(m_x + m_dx, m_y + m_dy)) {
+                                    let rendevous = 0;
+                                    let rendevous_complete = function() {
+                                        rendevous++;
+                                        if (rendevous === 2) {
+                                            change_state(STATES.POST_ATTACK_IDLE);
+                                        }
+                                    };
+                                    tweens.push(scene.tweens.add({
+                                        targets: { x: m_x},
+                                        props: { x: m_x+m_dx/2 },
+                                        duration: 100,
+                                        yoyo: true,
+                                        onUpdate: function() {
+                                            m_x = this.getValue();
+                                        },
+                                        onComplete: rendevous_complete
+                                    }));
+                                    tweens.push(scene.tweens.add({
+                                        targets: { y: m_y},
+                                        props: { y: m_y+m_dy/2 },
+                                        duration: 100,
+                                        yoyo: true,
+                                        onUpdate: function() {
+                                            m_y = this.getValue();
+                                        },
+                                        onComplete: rendevous_complete
+                                    }));
+                                    return;
+                                }
+                        }
+                        //so we didn't do it, return to idle
+                        change_state(STATES.IDLE);
+                        break;
+                    default:
+                        break;
+                }
+            };
+
+            let exit_state = function() {
+                switch(current_state) {
+                    default:
+                        break;
+                }
+            };
+
+            let change_state = function(new_state) {
+                //clear async calls
+                Phaser.Utils.Array.Each(tweens, function(tween) { tween.remove(); }, pawn);
+                Phaser.Utils.Array.Each(timelines, function(timeline) { timeline.stop(); }, pawn);
+                Phaser.Utils.Array.Each(delayedCalls, function(delayedCall) { delayedCall.remove(false); }, pawn);
+                tweens = [];
+                timelines = [];
+                delayedCalls = [];
+
+                exit_state();
+                current_state = new_state;
+                enter_state();
+            }
+
+            let enter_move = function() {
+                let directions_consts = {
+                    RIGHT: {dx: 1, dy: 0},
+                    LEFT: {dx: -1, dy: 0},
+                    UP: {dx: 0, dy: -1},
+                    DOWN: {dx: 0, dy: 1},
+                };
+                if (Phaser.Math.Between(0, 99) < 80) {
+                    m_prefer_horizontal = !m_prefer_horizontal;
+                }
+                let primary_vertical_index = 0;
+                let primary_horizontal_index = 1;
+                let secondary_vertical_index = 2;
+                let secondary_horizontal_index = 3;
+                let directions = [
+                    directions_consts.UP,
+                    directions_consts.LEFT,
+                    directions_consts.DOWN,
+                    directions_consts.RIGHT];
+                if (m_prefer_horizontal) {
+                    primary_horizontal_index = 0;
+                    primary_vertical_index = 1;
+                    secondary_horizontal_index = 2;
+                    secondary_vertical_index = 3;
+                    directions = [
+                        directions_consts.LEFT,
+                        directions_consts.UP,
+                        directions_consts.RIGHT,
+                        directions_consts.DOWN];
+                }
+                let deltaX = Math.round(playerX) - Math.round(m_x);
+                if (deltaX > 0 || (deltaX === 0 && Phaser.Math.Between(0, 99) < 50)) {
+                    //swap horizontals
+                    let temp = directions[primary_horizontal_index];
+                    directions[primary_horizontal_index] =
+                        directions[secondary_horizontal_index];
+                    directions[secondary_horizontal_index] = temp;
+                }
+                let deltaY = Math.round(playerY) - Math.round(m_y);
+                if (deltaY > 0 || (deltaY === 0 && Phaser.Math.Between(0, 99) < 50)) {
+                    //swap vertical
+                    let temp = directions[primary_vertical_index];
+                    directions[primary_vertical_index] =
+                        directions[secondary_vertical_index];
+                    directions[secondary_vertical_index] = temp;
+                }
+                //if we are really close we move away
+                if (Math.abs(deltaX) + Math.abs(deltaY) < 2) {
+                    Phaser.Utils.Array.RotateLeft(directions, 1);
+                };
+                m_dx = 0;
+                m_dy = 0;
+                for (let direction of directions) {
+                    if (isGridPassable(m_x+direction.dx, m_y+direction.dy)) {
+
+                        m_dx = direction.dx;
+                        m_dy = direction.dy;
+                        let rendevous = 0;
+                        let rendevous_complete = function() {
+                            rendevous++;
+                            if (rendevous === 2) {
+                                change_state(STATES.IDLE);
                             }
-                        });
+                        };
+                        tweens.push(scene.tweens.add({
+                            targets: { x: m_x},
+                            props: { x: m_x+m_dx },
+                            duration: 200,
+                            onUpdate: function() {
+                                m_x = this.getValue();
+                            },
+                            onComplete: rendevous_complete
+                        }));
+                        tweens.push(scene.tweens.add({
+                            targets: { y: m_y},
+                            props: { y: m_y+m_dy },
+                            duration: 200,
+                            onUpdate: function() {
+                                m_y = this.getValue();
+                            },
+                            onComplete: rendevous_complete
+                        }));
                         return;
                     }
                 }
-                delayedCall = scene.time.delayedCall(1000, begin_walk);
+                //no direction worked?
+                change_state(STATES.IDLE);
             };
-            delayedCall = scene.time.delayedCall(1000, begin_walk);
+
+            let bounding_box = scene.add.rectangle(0,0,GRID_SIZE-2,GRID_SIZE-2,0x00ff00,0.0);
+            let shadow = scene.add.ellipse(0, 0,
+                GRID_SIZE*.70,GRID_SIZE*.57,0x000000, 0.5);
 
             pawn.setData('update', function() {
-                pawn.setPosition(gridX(mx),characterY(my));
+                let deltaX = Math.round(playerX) - Math.round(m_x);
+                let deltaY = Math.round(playerY) - Math.round(m_y);
+                if (current_state === STATES.IDLE && playerMoveAllowed) {
+                    if ((deltaX === -1 && deltaY === -1) ||
+                        (deltaX === 1 && deltaY === 1) ||
+                        (deltaX === 1 && deltaY === -1) ||
+                        (deltaX === -1 && deltaY === 1)) {
+                        change_state(STATES.PRE_ATTACK);
+                    }
+                }
+                pawn.setPosition(gridX(m_x),characterY(m_y) + m_z);
+                shadow.setPosition(gridX(m_x),gridY(m_y));
+                pawn.setDepth(DEPTHS.ENTITIES + m_y);
+                bounding_box.setPosition(gridX(Math.round(m_x)),gridY(Math.round(m_y)));
             });
+
+            enter_state();
+            pawn.data.values.update();
             return pawn;
         };
 
@@ -348,6 +537,11 @@ let GameScene = new Phaser.Class({
         //----------------------------------------------------------------------
 
         scene.input.addPointer(5);
+
+        //----------------------------------------------------------------------
+        //SETUP PHYSICS
+        //----------------------------------------------------------------------
+
 
     },
 
